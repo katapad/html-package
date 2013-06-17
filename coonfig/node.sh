1544f3b785e49ffd6e7cd1d79a4c056f93b6ec6f
naoiwata <orrrizzle@gmail.com>
2013-06-17 20:46:50 +0900

    delete unneed files

--- coonfig/node.sh
+++
@@ -1,3 +0,0 @@
-# install node modules
-sudo npm install grunt  grunt-contrib-coffee  grunt-contrib-jade  grunt-contrib-less  grunt-contrib-stylus  grunt-contrib-uglify grunt-contrib-watch grunt-contrib-cssmin -g
-npm link grunt  grunt-contrib-coffee  grunt-contrib-jade  grunt-contrib-less  grunt-contrib-stylus  grunt-contrib-uglify grunt-contrib-watch grunt-contrib-cssmin
