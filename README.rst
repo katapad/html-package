a280076605f19e991e8200204a70da0c8d9a5a07
naoiwata <orrrizzle@gmail.com>
2013-09-06 15:21:36 +0900

    + create new branch for middleman

---
+++ README.rst
@@ -0,0 +1,11 @@
+html-package
+=============
+
+enable to develop static html pages
+------------------------------------
+
+version 0.0.1
+author naowata
+date 2013/9/6
+
+
--- config/install.sh
+++
@@ -1,6 +0,0 @@
-# install node modules
-
-sudo npm install grunt  grunt-contrib-coffee  grunt-contrib-jade  grunt-contrib-less  grunt-contrib-stylus  grunt-contrib-uglify grunt-contrib-watch grunt-contrib-cssmin grunt-ssh -g
-npm link grunt  grunt-contrib-coffee  grunt-contrib-jade  grunt-contrib-less  grunt-contrib-stylus  grunt-contrib-uglify grunt-contrib-watch grunt-contrib-cssmin grunt-ssh
-
-# END
